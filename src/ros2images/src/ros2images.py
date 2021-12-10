44be7ca5254f2ac6e13acdefe520f1d716ecd8fc
292213017 <18859275929@163.com>
2021-12-10 16:25:12 +0800

    第一次提交

---
+++ src/ros2images/src/ros2images.py
@@ -0,0 +1,23 @@
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+
+i = 0
+
+def callback(data):
+    bridge = CvBridge()
+    try:
+      img = bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+    name = "out/images_"+str(i)
+    cv2.imwrite('test.png',img)
+
+
+if __name__ == '__main__':
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("/d400/color/image_raw", Image, callback)
+    rospy.spin()
